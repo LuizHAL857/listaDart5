fbc73b5a9b4ee48eeabc7474269837116f475b56
Luiz Henrique <luizhenriquealveslima857@gmail.com>
2025-06-23 20:08:13 -0300

    Update main.dart

--- bin/main.dart
+++ bin/main.dart
@@ -1,1 +1,18 @@
+import 'package:aula5_exercicios/model/produto_alimenticio.dart';
+import 'package:aula5_exercicios/model/produto_eletronico.dart';
+import 'package:aula5_exercicios/service/carrinho_de_compras.dart';
+import 'package:aula5_exercicios/utils/json_handler.dart';
+
+void main() async {
+  final carrinho = CarrinhodeCompras();
+
+  final tv = ProdutoEletronico(codigo:'E01', nome:'Smart TV 50"', preco:2800.0, garantiaMeses:12);
+  final leite = ProdutoAlimenticio(codigo:'A01', nome:'Caixa de Leite', preco:4.5, dataDeValidade:"25/12/2025");
+
+  carrinho.adicionarProduto(tv);
+  carrinho.adicionarProduto(leite);
+
+  final caminho = 'carrinho.json';
+  await salvarCarrinhoEmJson(caminho, carrinho.toJsonList());
+}
 
